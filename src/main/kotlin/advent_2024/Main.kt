22dab8e24e14d865ac1c98200188656b6fda503f
Bastien Le Bret <blb@citygo.me>
2024-12-02 17:57:30 +0100

    Day 2 part 1

--- src/main/kotlin/advent_2024/Main.kt
+++ src/main/kotlin/advent_2024/Main.kt
@@ -1,9 +1,11 @@
 package advent_2024
 
 import advent_2024.puzzles.Day1.Day1Optimized
+import advent_2024.puzzles.Day2.Day2
 
 fun main(args: Array<String>) {
     //Day1Part1()
     //Day1Part2()
-    Day1Optimized()
+    //Day1Optimized()
+    Day2()
 }
